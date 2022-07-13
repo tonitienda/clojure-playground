46b9a625e09152c4508bea3712e56f987ea08092
Toni Tienda <tonitienda@gmail.com>
2022-07-13 22:50:26 +0900

    first spec that does not do what I expected

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject hello-world "0.1.0-SNAPSHOT"
   :main hello-world.core
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [
+      [org.clojure/clojure "1.5.1"]
+      [org.clojure/clojure "1.11.1"]
+  ])
--- src/hello_world/core.clj
+++ src/hello_world/core.clj
@@ -1,11 +1,18 @@
-(ns hello-world.core)
+(ns hello-world.core
+  (:require [clojure.spec.alpha :as s])
+)
 
 (defn add[x y]
   (+ x y)
 )
 
+(s/fdef add
+  :args (s/cat :x int :y int)
+  :ret int
+)
+
 (defn -main [& args]
-  (def result (add 1 4))
+  (def result (add 1 1.5))
   (def message (str "Hello, World!" result))
   (println message)
 )
