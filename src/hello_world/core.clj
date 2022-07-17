de8d90923ac51537dd851f09674726d692535fb5
Toni Tienda <tonitienda@gmail.com>
2022-07-17 19:16:30 +0900

    try s/def

--- src/hello_world/core.clj
+++ src/hello_world/core.clj
@@ -1,18 +1,30 @@
 (ns hello-world.core
-  (:require [clojure.spec.alpha :as s])
+  (:require 
+    [clojure.spec.alpha :as s]
+    [clojure.spec.test.alpha :as st])
 )
 
-(defn add[x y]
+(defn add-test[x y]
   (+ x y)
 )
 
-(s/fdef add
+(s/fdef hello-world.core/add-test
   :args (s/cat :x int :y int)
   :ret int
 )
 
+;; (st/instrument `add)
+;; (st/check `add)
+
+(s/fdef example-function :args (s/cat :x int))
+(st/instrument `example-function)
+(defn example-function [x]
+  (+ x 1)
+)
+
 (defn -main [& args]
-  (def result (add 1 1.5))
-  (def message (str "Hello, World!" result))
+  (def result (add-test 1 3))
+  (def result2 (example-function 3))
+  (def message (str "Hello, World!" result, result2))
   (println message)
 )
